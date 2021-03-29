4106b241132cb24025bd04703ceee1636294e2bf
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-03-29 22:21:37 +0900

    Add files via upload

---
+++ src/com/codingTest/B_1001.java
@@ -0,0 +1,13 @@
+package com.codingTest;
+
+import java.util.Scanner;
+
+public class B_1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A-B);
+    }
+}
