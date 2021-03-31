48cbd638158fb9260ad5f4100f43feea01016138
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-03-31 23:27:15 +0900

    Create B_10998.java

---
+++ src/BJOJ/B_10998.java
@@ -0,0 +1,12 @@
+package BJOJ;
+
+import java.util.Scanner;
+
+public class B_10998 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A*B);
+    }
+}
