32461635a9a959d29796c767dd482b4d6df01e06
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-04-06 23:19:12 +0900

    Update B_2588.java

--- src/BJOJ/B_2588.java
+++ src/BJOJ/B_2588.java
@@ -6,12 +6,11 @@ public class B_2588 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int A = scanner.nextInt();
-        String B = scanner.next();
-        System.out.println(B.charAt(2));
-        System.out.println((B.charAt(2)));
-        System.out.println(A*B.charAt(2));
-        System.out.println(A*B.charAt(1));
-        System.out.println(A*B.charAt(0));
-        System.out.println(A*Integer.parseInt(B));
+        int B = scanner.nextInt();
+        System.out.println(A * (B % 10));
+        System.out.println(A * ((B % 100) / 10));
+        System.out.println(A * ((B % 1000) / 100));
+        System.out.println(A*B);
     }
+
 }
