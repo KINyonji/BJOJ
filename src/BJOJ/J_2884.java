f3813c46ac1aa3083824567cb2edf94f6cb1a5c1
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-04-11 22:32:36 +0900

    Update J_2884.java

--- src/BJOJ/J_2884.java
+++ src/BJOJ/J_2884.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class J_2884 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int H = scanner.nextInt();
+        int M = scanner.nextInt();
 
+        if ((H >= 0 && H <= 23) && (M >= 0 && M <= 59)) {
+            if (M < 45) {
+                if (H!=0){
+                    System.out.println((H-1)+" "+(M+15));
+                }else {
+                    System.out.println(23+" "+(M+15));
+                }
+            } else if (M >= 45) {
+                System.out.println(H+" "+(M-45));
+            }
+        }
     }
 }
