6429598c027b3616fa11cdeb63fdc34418f5518e
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-04-14 22:37:51 +0900

    Create J_10950.java

---
+++ src/BJOJ/J_10950.java
@@ -0,0 +1,15 @@
+package BJOJ;
+
+import java.util.Scanner;
+
+public class J_10950 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int c = scanner.nextInt();
+        for (int i = 0; i < c; i++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
