26ee3d8de21f5b46da36870d58e9776308c006d7
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-04-15 23:53:14 +0900

    Update J_15552.java

--- src/BJOJ/J_15552.java
+++ src/BJOJ/J_15552.java
@@ -1,7 +1,20 @@
 package BJOJ;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class J_15552 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int testCase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCase; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write(A+B+"\n");
+        }
+        bw.flush();
     }
 }
