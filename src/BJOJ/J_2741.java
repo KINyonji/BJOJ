74efc18cd2242cdf30a4936c9dee0b5e100323d8
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-04-18 23:29:26 +0900

    Update J_2741.java

--- src/BJOJ/J_2741.java
+++ src/BJOJ/J_2741.java
@@ -1,15 +1,19 @@
 package BJOJ;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class J_2741 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
 
+        for (int i = 1; i <= N; i++) {
+            bw.write(i+"\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
