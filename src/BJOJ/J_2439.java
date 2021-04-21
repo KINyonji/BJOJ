e608498acea8f27699beb11bdedc6b2c8f73fd01
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-04-22 01:45:44 +0900

    Create J_2439.java

---
+++ src/BJOJ/J_2439.java
@@ -0,0 +1,24 @@
+package BJOJ;
+
+import java.io.*;
+
+public class J_2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N-i; j++) {
+                bw.write(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+
+        br.close();
+        bw.close();
+    }
+}
