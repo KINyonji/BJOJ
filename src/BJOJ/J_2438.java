53619ddd76fc8213c4e4f76cb41b0fec44a71771
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-04-22 01:45:49 +0900

    Update J_2438.java

--- src/BJOJ/J_2438.java
+++ src/BJOJ/J_2438.java
@@ -1,7 +1,21 @@
 package BJOJ;
 
+import java.io.*;
+
 public class J_2438 { //별 찍기
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        bw.close();
+        br.close();
     }
 }
