8c46e0b5519da97b725cba1b66a6410dec9cdd6e
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-05-01 00:11:54 +0900

    Update J_2577.java

--- src/BJOJ/J_2577.java
+++ src/BJOJ/J_2577.java
@@ -1,7 +1,24 @@
 package BJOJ;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class J_2577 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        int sum = A*B*C;
+        String num = String.valueOf(sum);
+        String[] arr = num.split("");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+//        int arr[] =
     }
 }
