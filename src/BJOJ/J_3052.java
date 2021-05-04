d63f4062f60e9158d2ce9d8a6383999bf1c9da63
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-05-04 22:01:40 +0900

    Create J_3052.java

---
+++ src/BJOJ/J_3052.java
@@ -0,0 +1,17 @@
+package BJOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class J_3052 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+
+    }
+}
