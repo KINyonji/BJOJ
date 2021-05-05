ba6b63ec8e0fce3484652d54b6773fd34b0f8c8e
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-05-05 23:02:28 +0900

    Create J_1546.java

---
+++ src/BJOJ/J_1546.java
@@ -0,0 +1,14 @@
+package BJOJ;
+
+import java.util.Scanner;
+
+public class J_1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); //시험 본 과목의 개수 입력
+
+        for (int i = 0; i < N; i++) {
+            
+        }
+    }
+}
