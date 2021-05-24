dc1e8233b40891085795f0b013fb1ebd5629c2b7
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-05-24 23:33:38 +0900

    Create J_11654.java

---
+++ src/BJOJ/J_11654.java
@@ -0,0 +1,10 @@
+package BJOJ;
+
+import java.io.IOException;
+
+public class J_11654 {
+    public static void main(String[] args) throws IOException {
+        int ASCII = System.in.read();
+        System.out.println(ASCII);
+    }
+}
