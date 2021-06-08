a06d22ab5ee325a0a9e1dd63d157fbcdedf20bcf
KINyonji <73460065+KINyonji@users.noreply.github.com>
2021-06-09 00:19:34 +0900

    Update J_2577.java

--- src/BJOJ/J_2577.java
+++ src/BJOJ/J_2577.java
@@ -8,5 +8,22 @@ public class J_2577 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int [] num = new int[30];
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num.length; i++) {
+            num[i]=A*B*C;
+            System.out.print(num[i]+",");
+        }
+        System.out.println();
+
+//        int[] count = new int[10];
+//
+//        for (int i = 0; i < M.len; i++) {
+//
+//        }
     }
 }
